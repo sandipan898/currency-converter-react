18b7955bab16414c97f3b61ec72ae3b722cf9a78
sandipan898 <sandipan.das898@gmail.com>
2021-01-10 22:37:23 +0530

    Create CurrencyRow

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
 import './App.css';
+import CurrencyRow from './CurrencyRow';
+
+const BASE_URL = "https://api.exchangeratesapi.io/latest"
 
 function App() {
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Convert</h1>
+      <CurrencyRow />
+      <div className="equals">=</div>
+      <CurrencyRow />
+    </>
   );
 }
 
